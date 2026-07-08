4d03c7a43b4d45090f16771b6ffc2bf0a52f3a8f
hildehammer20-netizen <hilde.hammer2.0@gmail.com>
2026-07-08 09:01:52 +0200

    Add files via upload

---
+++ leaderboard/leaderboard-data.js
@@ -0,0 +1,182 @@
+window.LEADERBOARD_DATA = [
+  {
+    "name": "Daniel",
+    "fullName": "Daniel",
+    "points": 131,
+    "place": 1,
+    "matchPoints": 67,
+    "bonusPoints": 64,
+    "exact": 10,
+    "avatar": "avatars/Daniel.png"
+  },
+  {
+    "name": "ThomasWB",
+    "fullName": "ThomasWB",
+    "points": 127,
+    "place": 2,
+    "matchPoints": 60,
+    "bonusPoints": 67,
+    "exact": 8,
+    "avatar": "avatars/ThomasWB.png"
+  },
+  {
+    "name": "Atle",
+    "fullName": "Atle",
+    "points": 121,
+    "place": 3,
+    "matchPoints": 60,
+    "bonusPoints": 61,
+    "exact": 8,
+    "avatar": "avatars/Atle.png"
+  },
+  {
+    "name": "Glenn",
+    "fullName": "Glenn",
+    "points": 121,
+    "place": 3,
+    "matchPoints": 60,
+    "bonusPoints": 61,
+    "exact": 8,
+    "avatar": "avatars/Glenn.png"
+  },
+  {
+    "name": "Eskil",
+    "fullName": "Eskil",
+    "points": 120,
+    "place": 5,
+    "matchPoints": 55,
+    "bonusPoints": 65,
+    "exact": 5,
+    "avatar": "avatars/Eskil.png"
+  },
+  {
+    "name": "Hilde",
+    "fullName": "Hilde Hammer",
+    "points": 118,
+    "place": 6,
+    "matchPoints": 59,
+    "bonusPoints": 59,
+    "exact": 8,
+    "avatar": "avatars/Hilde.png"
+  },
+  {
+    "name": "i",
+    "fullName": "i",
+    "points": 117,
+    "place": 7,
+    "matchPoints": 56,
+    "bonusPoints": 61,
+    "exact": 9,
+    "avatar": "avatars/i.png"
+  },
+  {
+    "name": "Øyvind Ludt",
+    "fullName": "Øyvind Ludt",
+    "points": 117,
+    "place": 7,
+    "matchPoints": 59,
+    "bonusPoints": 58,
+    "exact": 6,
+    "avatar": "avatars/Ludt.png"
+  },
+  {
+    "name": "Bente",
+    "fullName": "Bente",
+    "points": 115,
+    "place": 9,
+    "matchPoints": 50,
+    "bonusPoints": 65,
+    "exact": 5,
+    "avatar": "avatars/Bente.png"
+  },
+  {
+    "name": "Ruud",
+    "fullName": "Ruud",
+    "points": 114,
+    "place": 10,
+    "matchPoints": 57,
+    "bonusPoints": 57,
+    "exact": 9,
+    "avatar": "avatars/Ruud.png"
+  },
+  {
+    "name": "Martin",
+    "fullName": "Martin",
+    "points": 114,
+    "place": 10,
+    "matchPoints": 68,
+    "bonusPoints": 46,
+    "exact": 8,
+    "avatar": "avatars/Martin.png"
+  },
+  {
+    "name": "David",
+    "fullName": "David",
+    "points": 110,
+    "place": 12,
+    "matchPoints": 52,
+    "bonusPoints": 58,
+    "exact": 6,
+    "avatar": "avatars/david.png"
+  },
+  {
+    "name": "Kenneth",
+    "fullName": "Kenneth",
+    "points": 109,
+    "place": 13,
+    "matchPoints": 65,
+    "bonusPoints": 44,
+    "exact": 9,
+    "avatar": "avatars/Kenneth.png"
+  },
+  {
+    "name": "Klaus",
+    "fullName": "Klaus Eriksen",
+    "points": 108,
+    "place": 14,
+    "matchPoints": 53,
+    "bonusPoints": 55,
+    "exact": 5,
+    "avatar": "avatars/Klaus.png"
+  },
+  {
+    "name": "Trine",
+    "fullName": "Trine",
+    "points": 104,
+    "place": 15,
+    "matchPoints": 48,
+    "bonusPoints": 56,
+    "exact": 5,
+    "avatar": "avatars/Trine.png"
+  },
+  {
+    "name": "TT",
+    "fullName": "Thomas Trøa",
+    "points": 100,
+    "place": 16,
+    "matchPoints": 46,
+    "bonusPoints": 54,
+    "exact": 6,
+    "avatar": "avatars/TT.png"
+  },
+  {
+    "name": "Berger",
+    "fullName": "André Berger",
+    "points": 99,
+    "place": 17,
+    "matchPoints": 45,
+    "bonusPoints": 54,
+    "exact": 4,
+    "avatar": "avatars/Berger.png"
+  },
+  {
+    "name": "Finn",
+    "fullName": "Finn Egén Mobæk",
+    "points": 93,
+    "place": 18,
+    "matchPoints": 49,
+    "bonusPoints": 44,
+    "exact": 7,
+    "avatar": "avatars/Finn.png"
+  }
+];
